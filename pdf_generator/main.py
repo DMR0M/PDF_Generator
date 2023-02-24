437988f2e6109ffe08f59e29f8b4b2bed4ff6207
Rommel <rommeldm87@gmail.com>
2023-02-24 16:05:46 +0800

    Added footers for each page

--- pdf_generator/main.py
+++ pdf_generator/main.py
@@ -2,26 +2,44 @@
 from fpdf import FPDF
 import pandas as pd
 
+# Generating PDF in Python
 # PDF configurations
 pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv('../topics.csv')
 
 for idx, row in df.iterrows():
 	pdf.add_page()
 
+	# Set Header
 	# Font configurations
 	pdf.set_font(family='Times', style='B', size=24)
 	pdf.set_text_color(100, 100, 100)
 
+	# Create a pdf cell
 	pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
 
 	# x1 y1 x2 y2
 	pdf.line(10, 21, 200, 21)
 
+	# ln adds empty break lines
+	pdf.ln(260)
+
+	# Set Footer
+	pdf.set_font(family='Times', style='I', size=10)
+	pdf.set_text_color(180, 180, 180)
+	pdf.cell(w=0, h=12, txt=row['Topic'], align='R', ln=1)
+
 	# Iterate pages for each topic
 	for i in range(row['Pages'] - 1):
 		pdf.add_page()
+
+		# Add Footer
+		pdf.ln(270)
+		pdf.set_font(family='Times', style='I', size=10)
+		pdf.set_text_color(180, 180, 180)
+		pdf.cell(w=0, h=12, txt=row['Topic'], align='R', ln=1)
 
 
 pdf.output('output.pdf')
